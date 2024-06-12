d1068941faf41fce21c4e539e97c3e13ba2467ab
Nazief <duraninazief@gmail.com>
2024-06-12 04:32:20 +0200

    Starting new C# kurse

---
+++ CSharpMosh/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpMosh
+{
+    public enum ShippingMethode
+    {
+        RegulerAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethode.Express;
+            //Console.WriteLine((int)method);
+            //Console.WriteLine(method);   should find the different !!
+            var methodId = 3;
+            Console.WriteLine((ShippingMethode)methodId);
+        }
+    }
+
+
+}
--- ChalengeCodeSignal/Program.cs
+++ ChalengeCodeSignal/Program.cs
@@ -5,3 +5,13 @@
         return (int)(Math.Pow(n, 2) + Math.Pow((n-1), 2));
     }
 }
+int MakeArrayConsecutive(int[] statues) {
+    int mx = statues.Max();
+    int mn = statues.Min();
+    int result = 0;
+    for(int i =mn; i<mx; i++){
+        if(!statues.Contains(i)){
+        result+=1;
+        }
+    }return result;
+}
