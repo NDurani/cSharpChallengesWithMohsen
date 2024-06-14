5ba210e230eeb59e2830994ef68286ea9ae968cf
Nazief <duraninazief@gmail.com>
2024-06-14 03:36:13 +0200

    people and trees Array sorting compeleted

--- ChalengeCodeSignal/Program.cs
+++ ChalengeCodeSignal/Program.cs
@@ -14,4 +14,30 @@ int MakeArrayConsecutive(int[] statues) {
         result+=1;
         }
     }return result;
+
+//sorting list and People
+
+int[] solution(int[] a) {
+int[] result = new int[a.Length];
+List<int> sorted_list = new List<int>{};
+for(int i = 0; i<a.Length; i++){
+    if(a[i] == -1){
+        result[i] = -1;
+    }
+    else{
+        sorted_list.Add(a[i]);
+    }
+}
+sorted_list.Sort();
+int counter = 0;
+for(int i = 0; i<result.Length;i++){
+    if(result[i] == -1){
+        result[i] = -1;
+    }else{
+        result[i] = sorted_list[counter];
+        counter++;
+    }
+}return result;
+}
+
 }
