f024e5b1040aadaef59e7ae65110f2fa5b173a7e
Nazief <duraninazief@gmail.com>
2024-06-20 20:18:17 +0200

    Learn C# and have send some applacation

---
+++ MyProject/MyArr.cs
@@ -0,0 +1,11 @@
+namespace myproject{
+
+public class MyArr
+{
+    
+    public int[]? FavNums { get; set; } 
+    public string[] Customers  { get; set;}
+    public string[] Employees { get; set; }
+    public object[] obj { get; set; }
+}
+}
---
+++ MyProject/Program.cs
@@ -0,0 +1,22 @@
+﻿// using MyArr;
+namespace myproject{
+
+public class Program{
+
+public static void Main(string[] args){
+    MyArr myarr = new MyArr();
+    myarr.FavNums = new int[]{5, 10, 15, 20, 25, 30};
+    myarr.Customers = new string[]{"Bob", "Sally", "Sue"};
+    myarr.Employees = new string[]{"Mike", "Paul", "Rick"};
+    myarr.obj = new object[] {12, "ahmad", true};
+
+    string[,] custNames = new string[2, 2] { { "Bob", "Smith" }, { "Sally", "Smith" } };
+    
+System.Console.WriteLine(custNames[0,0]);
+
+
+    // System.Console.WriteLine(myarr.FavNums[2] + "\n" + myarr.Customers[1] + " " + myarr.obj[1].GetType());
+}
+
+}
+}
